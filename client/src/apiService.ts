6fe5fe30c9f3e3089680036db1dbfbeadc78d5c8
ascolm <mertascioglu@gmail.com>
2021-05-08 23:07:02 +0300

    refactor(apiservice): create apiHelper function to wrap api requests

--- client/src/apiService.ts
+++ client/src/apiService.ts
@@ -1,5 +1,15 @@
 import config from 'config';
 
+const apiHelper = async (fetcher: () => Promise<Response>, errorMsg: string = 'An error ocurred in API request.') => {
+  const response = await fetcher();
+  if (response.ok) {
+    const parsedResponse = await response.json();
+    return parsedResponse;
+  } else {
+    throw new Error(errorMsg);
+  }
+};
+
 export const getSongs = async () => {
   const response = await fetch(config.apiBaseUrl + config.apiSongUrl);
   if (response.ok) {
