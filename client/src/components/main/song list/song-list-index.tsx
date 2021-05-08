f9a4228074a29e3a24a15ce7ee1336a5424b057a
ascolm <mertascioglu@gmail.com>
2021-05-08 16:35:24 +0300

    feat(song-list): add useEffect hook to fetch songs on load

--- client/src/components/main/song list/song-list-index.tsx
+++ client/src/components/main/song list/song-list-index.tsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Song } from 'interfaces/interfaces';
+import { getSongs } from 'apiService';
 import styles from './song-list.module.scss';
 
 export interface Props {
-
 }
 
 const SongList: React.FC<Props> = (props: Props) => {
+  let [songList, setSongList] = useState<Song[]>([]);
+
+  useEffect(() => {
+    const asyncSongFetcher = async () => {
+      const songList = await getSongs();
+      setSongList(songList);
+    };
+
+    asyncSongFetcher();
+  }, []);
+
   return (
     <div className={styles['song-list-container']}>
+      {songList.length > 0 && songList.map(s => <p>{s.name}</p>)}
     </div>
   );
 };
