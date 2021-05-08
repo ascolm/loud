d84ba9f57ba885a8e270ee5145a893020ad0651f
ascolm <mertascioglu@gmail.com>
2021-05-08 15:13:20 +0300

    build(client): create config file & link to env vars

---
+++ client/src/config.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  apiBaseUrl: process.env.REACT_APP_API_BASE_URL,
+  apiSongUrl: process.env.REACT_APP_API_SONG_URL,
+  apiLikeUrl: process.env.REACT_APP_API_LIKE_URL
+};
+
+export default config;
